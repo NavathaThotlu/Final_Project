140afe56f84d375901a8c3130301c89c9282b991
NavathaThotlu <118002905+NavathaThotlu@users.noreply.github.com>
2022-12-05 17:44:41 +0530

    Add files via upload

---
+++ TravelBlog/MiniProjectApp/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using MiniProjectApp.DataModels;
+using MiniProjectApp.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MiniProjectApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            var Login = new LoginView();
+            return View(Login);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginView postedData)
+        {
+            if (ModelState.IsValid)
+            {
+                var context = new COMPANYEntities();
+                var user = context.UserTs.Where(cust => cust.User_Name == postedData.UserName && cust.Password == postedData.password).FirstOrDefault();
+                if(user!=null)
+                {
+                    Session["UserData"] = user;
+                }
+                else if (user == null)
+                {
+                    ModelState.AddModelError("Login Failure ","Login failed for the user");
+                    return View(postedData);
+                }
+                
+                FormsAuthentication.SetAuthCookie(postedData.UserName, false);
+                FormsAuthentication.RedirectFromLoginPage(postedData.password, false);
+                return View(postedData);
+            }
+            else
+            {
+                return View(postedData);
+            }
+        }
+        public ActionResult Register()
+        {
+            var UserDetails = new UserDetailsVN();
+            return View(UserDetails);
+        }
+       
+        [HttpPost]
+        public ActionResult Register(UserDetailsVN user)
+        {
+            if (ModelState.IsValid && isValidUser(user.UserName))
+            {
+                var _context = new COMPANYEntities();
+                var UserTableRow = new UserT
+                {
+                    User_Name=user.UserName,
+                    Name=user.Name,
+                    Address=user.Address,
+                    MobileName=user.mobileNo,
+                    EmailAddress=user.email,
+                    Password=user.password
+                };
+                _context.UserTs.Add(UserTableRow);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("Failure ","UserName already exists");
+                return View();
+            }
+        }
+        public bool isValidUser(string UserName)
+        {
+            var _context = new COMPANYEntities();
+            var selected = _context.UserTs.SingleOrDefault(user => user.User_Name == UserName);
+            return selected == null ? true : false;
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TravelBlog/MiniProjectApp/Controllers/BloggingController.cs
@@ -0,0 +1,81 @@
+﻿using MiniProjectApp.Models;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web.Mvc;
+
+namespace MiniProjectApp.Controllers
+{
+    [Authorize]
+    public class BloggingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProfileData()
+        {
+            var data = new Blogt();
+            return View(data);
+        }
+        public ActionResult AddBlogs()
+        {
+            if (Session["UserData"]==null)
+            {
+                TempData["ErrorInfo"] = "Please login before adding blogs";
+                TempData.Keep();
+            }
+            var user = Session["UserData"] as UserT;
+            var blog = new Blogt();
+            blog.Userid = user.User_Id;
+            blog.UserT= user; ;
+            return View(blog);
+        }
+        [HttpPost]
+        public ActionResult AddBlogs(Blogt blogsTable)
+        {
+            var context = new COMPANYEntities();
+            context.Blogts.Add(blogsTable);
+            context.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult DisplayBlogs()
+        {
+            var context = new COMPANYEntities();
+            var data = context.Blogts.ToList();
+            return View(data);
+        }
+        public ActionResult EditBlogs(string Id)
+        {
+            var context = new COMPANYEntities();
+           var BId=int.Parse(Id);
+            var selected=context.Blogts.SingleOrDefault(b=>b.BlogId==BId);
+            return View(selected);
+        }
+        [HttpPost]
+        public ActionResult UpdateBlogs(Blogt blogsTable)
+        {
+            var context = new COMPANYEntities();
+            var selected = context.Blogts.SingleOrDefault(b => b.BlogId == blogsTable.BlogId);
+            if(selected==null)
+            {
+                throw new System.Exception("Blog is not found");
+            }
+            selected.Title = blogsTable.Title;
+            selected.Userid = blogsTable.Userid;
+            selected.Images = blogsTable.Images;
+            selected.Details= blogsTable.Details;
+            selected.ratings= blogsTable.ratings;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteBlogs(string Id) 
+        {
+            var context=new COMPANYEntities();
+            var BId = int.Parse(Id);
+            var model = context.Blogts.SingleOrDefault(b=>b.BlogId==BId);
+            context.Blogts.Remove(model);  
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
